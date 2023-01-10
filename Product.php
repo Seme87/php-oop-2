84be4226baf62efe4ca81d394ce59a4293208b06
Seme87 <massimomonteleone@hotmail.com>
2023-01-10 16:10:28 +0100

    first commit

---
+++ Product.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ ."Category.php";
+
+
+class Product {
+    public $name;
+    public $price;
+    public $category;
+
+    public function __construct($name, $price, $category)
+    {
+        $this->name= $name;
+        $this->price=$price;
+        $this->category= $category;
+    }
+}
+?>
