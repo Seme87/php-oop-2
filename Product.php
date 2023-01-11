cfa098fff952e954228e29c71806f832cc1e1633
Seme87 <massimomonteleone@hotmail.com>
2023-01-11 15:23:54 +0100

    Ho creato un trait da utilizzare in product

--- Product.php
+++ Product.php
@@ -1,14 +1,15 @@
 <?php
+require_once __DIR__."/traits/Name.php";
 
 class Product {
-    public $name;
+    use Name;
     public $price;
     public $category;
     public $immagine;
 
     public function __construct($name, $price, $category, $immagine)
     {
-        $this->name= $name;
+        
         $this->price=$price;
         $this->category= $category;
         $this->immagine=$immagine;
---
+++ traits/Name.php
@@ -0,0 +1,8 @@
+<?php 
+trait Name {
+    public $name;
+
+    public function __construct($name){
+        $this->name=$name;
+    }
+};
